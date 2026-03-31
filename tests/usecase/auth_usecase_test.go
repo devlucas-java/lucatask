cbdb981eebf61704dcf64a875d087f8484b6da1c
devlucas-java <devlucasjava@gmail.com>
2026-03-31 17:14:12 +0200

    feat: add auth and task usecase tests

---
+++ tests/usecase/auth_usecase_test.go
@@ -0,0 +1,98 @@
+package usecase
+
+import (
+	"testing"
+
+	"github.com/devlucas-java/lucatask/internal/delivery/dto"
+	"github.com/devlucas-java/lucatask/internal/domain"
+	"github.com/devlucas-java/lucatask/internal/infra/database"
+	"github.com/devlucas-java/lucatask/internal/usecase"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestLogin(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.User{})
+
+	repo := database.NewUserDB(db)
+	usecase := usecase.NewAuthUseCase(repo)
+
+	hash, err := bcrypt.GenerateFromPassword([]byte("password"), bcrypt.DefaultCost)
+	assert.NoError(t, err)
+
+	user := domain.User{
+		Name:     "name",
+		Email:    "login",
+		Password: string(hash),
+	}
+	_ = repo.Create(&user)
+
+	dto, err := usecase.Login("login", "password")
+	assert.NoError(t, err)
+	assert.NotNil(t, dto)
+	assert.Equal(t, dto.User.Email, user.Email)
+	assert.Equal(t, dto.User.Name, user.Name)
+	assert.NotEmpty(t, dto.Token)
+}
+
+func TestLoginInvalidCredentials(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.User{})
+
+	repo := database.NewUserDB(db)
+	usecase := usecase.NewAuthUseCase(repo)
+
+	_, err = usecase.Login("nonexistent", "password")
+	assert.Error(t, err)
+}
+
+func TestRegisterSuccessfully(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.User{})
+
+	repo := database.NewUserDB(db)
+	usecase := usecase.NewAuthUseCase(repo)
+	dtoRegister := dto.RegisterDTO{
+		Name:     "name",
+		Email:    "email",
+		Password: "password",
+	}
+
+	dto, err := usecase.Register(&dtoRegister)
+	assert.NoError(t, err)
+	assert.NotNil(t, dto)
+	assert.Equal(t, dto.User.Email, "email")
+	assert.Equal(t, dto.User.Name, "name")
+	assert.NotEmpty(t, dto.Token)
+}
+
+func TestRegisterDuplicateEmail(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.User{})
+
+	repo := database.NewUserDB(db)
+	usecase := usecase.NewAuthUseCase(repo)
+
+	dtoRegister := dto.RegisterDTO{
+		Name:     "name",
+		Email:    "email",
+		Password: "password",
+	}
+
+	_, err = usecase.Register(&dtoRegister)
+	assert.NoError(t, err)
+
+	_, err = usecase.Register(&dtoRegister)
+	assert.Error(t, err)
+}
---
+++ tests/usecase/task_usecase_test.go
@@ -0,0 +1,98 @@
+package usecase
+
+import (
+	"testing"
+
+	"github.com/devlucas-java/lucatask/internal/delivery/dto"
+	"github.com/devlucas-java/lucatask/internal/domain"
+	"github.com/devlucas-java/lucatask/internal/infra/database"
+	"github.com/devlucas-java/lucatask/internal/usecase"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func TestUpdateUser(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.User{})
+
+	repo := database.NewUserDB(db)
+	usecase := usecase.NewUserUseCase(repo)
+
+	user := domain.User{
+		Name:     "name",
+		Email:    "email",
+		Password: "password",
+	}
+	err = repo.Create(&user)
+	assert.NoError(t, err)
+
+	dto := dto.UserUpdateDTO{
+		Name:     "new name",
+		Email:    "new email",
+		Password: "new password",
+	}
+
+	usecase.UpdateUser(user.ID.String(), &dto)
+
+	userFound, err := repo.FindByID(user.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, userFound)
+	assert.Equal(t, userFound.ID, user.ID)
+	assert.NotEqual(t, userFound.Name, dto.Name)
+	assert.Equal(t, "new email", dto.Email)
+
+}
+
+func TestDeleteTask(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.Task{})
+
+	repo := database.NewTaskDB(db)
+	usecase := usecase.NewTaskUseCase(repo)
+
+	task := domain.Task{
+		Name:        "title",
+		Description: "body",
+	}
+	err = repo.Create(&task)
+	assert.NoError(t, err)
+
+	usecase.DeleteTask(task.ID.String())
+
+	taskFound, err := repo.FindByID(task.ID)
+	assert.Error(t, err)
+	assert.Nil(t, taskFound)
+}
+
+func TestCompletedTask(t *testing.T) {
+	db, err := gorm.Open(sqlite.Open(":memory:"), &gorm.Config{})
+	assert.NoError(t, err)
+
+	db.AutoMigrate(&domain.Task{})
+
+	repo := database.NewTaskDB(db)
+	usecase := usecase.NewTaskUseCase(repo)
+
+	task := domain.Task{
+		Name:        "title",
+		Description: "body",
+	}
+	err = repo.Create(&task)
+	assert.NoError(t, err)
+
+	dto := dto.TaskCompletedDTO{
+		Completed: true,
+	}
+	usecase.CompletedTask(task.ID.String(), &dto)
+
+	taskFound, err := repo.FindByID(task.ID)
+	assert.NoError(t, err)
+	assert.NotNil(t, taskFound)
+	assert.Equal(t, taskFound.ID, task.ID)
+	assert.Equal(t, taskFound.Completed, true)
+}
